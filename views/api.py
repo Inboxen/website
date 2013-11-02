0630d9a454a9db35888baba8459b469b216bb49d
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-11-02 22:53:31 +0000

    Use django-tastypie, nom nom nom

--- views/api.py
+++ views/api.py
@@ -15,90 +15,96 @@
 #    along with Inboxen.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
-import json
+"""
+Parts of this file have been taken from TastyPie[0], and thus the following terms
+apply to them:
 
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
+    Copyright (c) 2010, Daniel Lindsley
+    All rights reserved.
 
-from inboxen.models import Email, Inbox, Header
-from website.views.error import permission_denied
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+        * Redistributions of source code must retain the above copyright
+          notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of the tastypie nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
 
-@login_required
-@require_http_methods(['GET', 'HEAD'])
-def count(request, inbox=None, domain=None):
-    """Count the number of unread messages in an inbox"""
-    emails = grab_emails(request, inbox, domain)
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL tastypie BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-    output = {'count': emails.filter(read=False).count()}
+[0] https://github.com/toastdriven/django-tastypie
+"""
 
-    return render_api(output)
+from django.utils.translation import ugettext as _
+from tastypie.authentication import SessionAuthentication
+from tastypie.authorization import DjangoAuthorization
+from tastypie.resources import ModelResource, Resource
 
-@login_required
-@require_http_methods(['GET', 'HEAD', 'DELETE'])
-def emails(request, inbox=None, domain=None):
-    """
-    GET: Get a list of emails that have come in since last page load (or status=304 if none)
-    HEAD: Find out if there are new messages (status=200 if yes, 304 if no)
-    DELETE: Delete an email"""
+from inboxen.models import Inbox
+from queue.delete.tasks import delete_inbox
 
-    emails = grab_emails(request, inbox, domain)
+class InboxesResource(ModelResource):
+    """Handle Inbox related requests"""
+    #TODO: make tags updatable as a child of this resource?
+    #TODO: allow creation of new Inbox
 
-    if request.method in ['GET','HEAD']:
-        #check if-modified-since header
-        since = None # some datetime object with tz info
-        emails = emails.filter(recieved_date__gte=since)
+    def dehydrate(self, bundle):
+        bundle.data['unread'] = bundle.obj.email_set.filter(read=False).count()
+        return bundle
 
-        # HEAD responses have no body
-        if request.method == 'HEAD':
-            if emails.exist():
-                return HttpResponse("", "application/json", 200)
-            else:
-                return HttpResponse("", "application/json", 304)
+    def obj_delete(self, bundle, **kwargs):
+        """Delete object, partly taken from TastyPie source"""
+        if not hasattr(bundle.obj, 'delete'):
+            try:
+                bundle.obj = self.obj_get(bundle=bundle, **kwargs)
+            except Inbox.DoesNotExist:
+                raise NotFound(_("Inbox not found"))
 
-        # ... GET responses do
-        elif request.method == 'GET':
-            emails = emails.order_by("-recieved_date").defer('body')
-            output = []
-            for email in emails:
-               row = {}
+        self.authorized_delete_detail(self.get_object_list(bundle.request), bundle)
 
-                row['id'] = email.eid
-                row['read'] = email.read
+        bundle.obj.deleted = True
+        bundle.obj.save()
+        delete_inbox.delay(bundle.obj)
 
-                try:
-                    row['subject'] = email.headers.get(name='subject')
-                except Header.DoesNotExist:
-                    row['subject'] = ""
+    def obj_delete_list(self, bundle, **kwargs):
+        """Delete list of objects, partly taken from TastyPie source"""
+        objects_to_delete = self.obj_get_list(bundle=bundle, **kwargs)
+        deletable_objects = self.authorized_delete_list(objects_to_delete, bundle)
 
-                try:
-                    row['form'] = email.headers.get(name='form')
-                except Header.DoesNotExist:
-                    row['form'] = ""
+        for authed_obj in deletable_objects:
+            authed_obj.deleted = True
+            authed_obj.save()
+            delete_inbox.delay(authed_obj)
 
-                output.append(row)
+    class Meta:
+        resource_name = 'inbox'
+        queryset = Inbox.objects.filter(deleted=False)
+        list_allowed_methods = ['get', 'delete']
+        exclude =['deleted', 'user']
 
-            return render_api(output, 200)
+        authentication = SessionAuthentication()
+        authorization = InboxenAuth()
 
-    elif request.method == 'DELETE':
-        pass
+class EmailsResource(ModelResource):
+    """Handle Email related requests"""
+    #TODO: implement
 
-def grab_emails(request, inbox, domain):
-    #TODO: Move this in with the other helpers
-    if inbox and domain:
-        try:
-            inbox = Inbox.objects.only('id').get(inbox=inbox, domain__domain=domain, deleted=False, user=request.user)
-        except Inbox.DoesNotExist:
-            return render_api({'error':"Permission denied"}, 403)
+    pass
 
-        emails = Email.objects.filter(inbox=inbox)
+class InboxenAuth(DjangoAuthorization):
+    """Do Inboxen style user checking"""
+    #TODO: implement
 
-    else:
-        emails = Email.objects.filter(user=request.user)
-
-    return emails
-
-def render_api(data, status=200):
-    """Renders json and creates a HttpResponse object"""
-    data = json.dumps(data)
-    return HttpResponse(data, "application/json", status)
+    pass
